5a629707b1a7987f53c1664b331d2be0dd028922
Eric Langlois <eric@langlois.xyz>
2021-02-14 18:28:15 -0500

    Implement Vec3 as a struct not array

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,26 +1,18 @@
 use num_traits::real::Real;
 use std::fmt;
-use std::ops::{
-    Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Neg, Sub, SubAssign,
-};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 
 /// 3-dimensional vector
 #[derive(Debug)]
-pub struct Vec3<T>([T; 3]);
+pub struct Vec3<T> {
+    pub x: T,
+    pub y: T,
+    pub z: T,
+}
 
 impl<T> Vec3<T> {
     pub fn new(x: T, y: T, z: T) -> Self {
-        Self([x, y, z])
-    }
-
-    pub fn x(&self) -> &T {
-        &self.0[0]
-    }
-    pub fn y(&self) -> &T {
-        &self.0[1]
-    }
-    pub fn z(&self) -> &T {
-        &self.0[2]
+        Self { x, y, z }
     }
 
     /// Inner product
@@ -29,11 +21,9 @@ impl<T> Vec3<T> {
         T: Mul<U>,
         <T as Mul<U>>::Output: AddAssign,
     {
-        let [x, y, z] = self.0;
-        let [ox, oy, oz] = other.0;
-        let mut value = x * ox;
-        value += y * oy;
-        value += z * oz;
+        let mut value = self.x * other.x;
+        value += self.y * other.y;
+        value += self.z * other.z;
         value
     }
 }
@@ -46,9 +36,11 @@ impl<T: Copy> Vec3<T> {
         U: Copy,
         <T as Mul<U>>::Output: Sub,
     {
-        let [x, y, z] = self.0;
-        let [ox, oy, oz] = other.0;
-        Vec3([y * oz - z * oy, z * ox - x * oz, x * oy - y * ox])
+        Vec3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
     }
 }
 
@@ -79,7 +71,7 @@ where
 
 impl<T: fmt::Display> fmt::Display for Vec3<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {}, {})", self.0[0], self.0[1], self.0[2])
+        write!(f, "({}, {}, {})", self.x, self.y, self.y)
     }
 }
 
@@ -87,36 +79,44 @@ impl<T: Copy> Copy for Vec3<T> {}
 
 impl<T: Clone> Clone for Vec3<T> {
     fn clone(&self) -> Self {
-        Vec3([self.0[0].clone(), self.0[1].clone(), self.0[2].clone()])
+        Vec3 {
+            x: self.x.clone(),
+            y: self.y.clone(),
+            z: self.z.clone(),
+        }
     }
 }
 
-impl<T, I> Index<I> for Vec3<T>
-where
-    [T]: Index<I>,
-{
-    type Output = <[T] as Index<I>>::Output;
+// impl<T, I> Index<I> for Vec3<T>
+// where
+//     [T]: Index<I>,
+// {
+//     type Output = <[T] as Index<I>>::Output;
 
-    fn index(&self, index: I) -> &Self::Output {
-        self.0.index(index)
-    }
-}
+//     fn index(&self, index: I) -> &Self::Output {
+//         self.0.index(index)
+//     }
+// }
 
-impl<T, I> IndexMut<I> for Vec3<T>
-where
-    [T]: IndexMut<I>,
-{
-    fn index_mut(&mut self, index: I) -> &mut Self::Output {
-        self.0.index_mut(index)
-    }
-}
+// impl<T, I> IndexMut<I> for Vec3<T>
+// where
+//     [T]: IndexMut<I>,
+// {
+//     fn index_mut(&mut self, index: I) -> &mut Self::Output {
+//         self.0.index_mut(index)
+//     }
+// }
 
 impl<T: Neg> Neg for Vec3<T> {
     type Output = Vec3<<T as Neg>::Output>;
 
     fn neg(self) -> Self::Output {
-        let [x, y, z] = self.0;
-        Vec3([-x, -y, -z])
+        let Vec3 { x, y, z } = self;
+        Vec3 {
+            x: -x,
+            y: -y,
+            z: -z,
+        }
     }
 }
 
@@ -125,19 +125,20 @@ impl<T: Add<U>, U> Add<Vec3<U>> for Vec3<T> {
 
     /// Vector addition
     fn add(self, other: Vec3<U>) -> Self::Output {
-        let [x, y, z] = self.0;
-        let [ox, oy, oz] = other.0;
-        Vec3([x + ox, y + oy, z + oz])
+        Vec3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
     }
 }
 
 impl<T: AddAssign<U>, U> AddAssign<Vec3<U>> for Vec3<T> {
     /// Vector addition in-place
     fn add_assign(&mut self, other: Vec3<U>) {
-        let [ox, oy, oz] = other.0;
-        self.0[0] += ox;
-        self.0[1] += oy;
-        self.0[2] += oz;
+        self.x += other.x;
+        self.y += other.y;
+        self.z += other.z;
     }
 }
 
@@ -146,19 +147,20 @@ impl<T: Sub<U>, U> Sub<Vec3<U>> for Vec3<T> {
 
     /// Vector subtraction
     fn sub(self, other: Vec3<U>) -> Self::Output {
-        let [x, y, z] = self.0;
-        let [ox, oy, oz] = other.0;
-        Vec3([x - ox, y - oy, z - oz])
+        Vec3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
     }
 }
 
 impl<T: SubAssign<U>, U> SubAssign<Vec3<U>> for Vec3<T> {
     /// Vector subtaction in-place
     fn sub_assign(&mut self, other: Vec3<U>) {
-        let [ox, oy, oz] = other.0;
-        self.0[0] -= ox;
-        self.0[1] -= oy;
-        self.0[2] -= oz;
+        self.x -= other.x;
+        self.y -= other.y;
+        self.z -= other.z;
     }
 }
 
@@ -171,8 +173,11 @@ where
 
     /// Scalar multiplication
     fn mul(self, t: U) -> Self::Output {
-        let [x, y, z] = self.0;
-        Vec3([x * t, y * t, z * t])
+        Vec3 {
+            x: self.x * t,
+            y: self.y * t,
+            z: self.z * t,
+        }
     }
 }
 
@@ -183,9 +188,9 @@ where
 {
     /// Scalar multiplication in-place
     fn mul_assign(&mut self, t: U) {
-        self.0[0] *= t;
-        self.0[1] *= t;
-        self.0[2] *= t;
+        self.x *= t;
+        self.y *= t;
+        self.z *= t;
     }
 }
 
@@ -198,8 +203,11 @@ where
 
     /// Scalar division
     fn div(self, t: U) -> Self::Output {
-        let [x, y, z] = self.0;
-        Vec3([x / t, y / t, z / t])
+        Vec3 {
+            x: self.x / t,
+            y: self.y / t,
+            z: self.z / t,
+        }
     }
 }
 
@@ -210,8 +218,8 @@ where
 {
     /// Scalar division in-place
     fn div_assign(&mut self, t: U) {
-        self.0[0] /= t;
-        self.0[1] /= t;
-        self.0[2] /= t;
+        self.x /= t;
+        self.y /= t;
+        self.z /= t;
     }
 }
