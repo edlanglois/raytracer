3130340bc40fc342a5e20adf82b3b034fedb0ba8
Eric Langlois <eric@langlois.xyz>
2021-02-16 17:40:20 -0500

    Set short flags for all CLI options

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,13 @@ pub struct Opts {
     #[clap(short, long, default_value = "3:2")]
     pub aspect_ratio: Ratio<u32>,
 
-    #[clap(long, default_value = "400")]
+    #[clap(short, long, default_value = "400")]
     pub width: u32,
 
     #[clap(short, long, default_value = "100")]
     pub samples_per_pixel: u32,
 
-    #[clap(long, default_value = "50")]
+    #[clap(short('d'), long, default_value = "50")]
     pub max_depth: u32,
 }
 
